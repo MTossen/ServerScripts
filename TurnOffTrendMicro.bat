3202c856335342ff9e6fbab4e3b727658c4e4f60
MTossen <75661688+MTossen@users.noreply.github.com>
2021-09-01 00:41:12 +0200

    Create TurnOffTrendMicro.bat

---
+++ TurnOffTrendMicro.bat
@@ -0,0 +1,8 @@
+Trend Micro
+CD "C:\program files\trend micro\deep security agent\" 
+dsa_control -s 0
+
+
+Symantec
+CD "C:\Program Files (x86)\Symantec\Symantec Endpoint Protection"
+SMC.EXE -STOP
