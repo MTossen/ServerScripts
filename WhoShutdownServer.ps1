a7f28645b2d6f483f657050fa2eb764bb2b25532
MTossen <75661688+MTossen@users.noreply.github.com>
2021-09-01 00:42:31 +0200

    Create WhoShutdownServer.ps1

---
+++ WhoShutdownServer.ps1
@@ -0,0 +1,10 @@
+# Run on the server you want to check.
+
+$properties = @(
+    @{n='TimeStamp';e={$_.TimeCreated}},
+    @{n='User';e={$_.Properties[6].Value}},
+    @{n='Reason/Application';e={$_.Properties[0].Value}},
+    @{n='Action';e={$_.Properties[4].Value}}
+)
+Get-WinEvent -FilterHashTable @{LogName='System'; ID=1074} | 
+Select $properties | Sort-Object "$_.TimeCreated"
